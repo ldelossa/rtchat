18c38fc5184d62a27c01b34086e1dce7fb69be7f
Louis DeLosSantos <ldelossa.ld@gmail.com>
2018-03-19 01:06:03 -0400

    dockerization and deployment, wrap chatserver in jwt

--- cmd/userservice/main.go
+++ cmd/userservice/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	us "github.com/ldelossa/rtchat/userservice"
 
 	pg "github.com/ldelossa/rtchat/userservice/postgres"
 )
 
-var Addr = "localhost:8081"
+const (
+	Addr = "0.0.0.0:8080"
+)
+
+// Default db connection string expecting postgres to be running on your localhost at default port.
+// Use POSTGRESCONNSTR env variable to overwrite
 var ConnString = "user=postgres dbname=userservice password=dev host=localhost sslmode=disable"
 
 func main() {
+
+	// Overwrite ConnStr if environemnt varible "POSTGRESCONNSTR" exists
+	if os.Getenv("POSTGRESCONNSTR") != "" {
+		ConnString = os.Getenv("POSTGRESCONNSTR")
+	}
 
 	// Create DataStore
 	ds, err := pg.NewDatastore(ConnString)
